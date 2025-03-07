41afefc26d134c65a515b1c382c5839b048fc0ce
Pedro Antunes <pedronmantunes@gmail.com>
2025-03-07 14:20:17 +0100

    Add AXI interface

--- HLS/VAENet/vae_ip/script.tcl
+++ HLS/VAENet/vae_ip/script.tcl
@@ -9,7 +9,7 @@ add_files -tb vaemodel1_hls_test.cpp
 open_solution "solution1"
 set_part {xczu7ev-ffvc1156-2-i}
 create_clock -period 5 -name default
-config_interface -mode m_axi -slave -datawidth 32 -addrwidth 32
+config_interface -m_axi_alignment_byte_size 64 -m_axi_latency 64 -m_axi_max_widen_bitwidth 512
 csynth_design
 export_design -format ip_catalog -description "Ekaterina VAE encoder accelerator" -display_name "VAENet (encoder accelerator)"
 exit
